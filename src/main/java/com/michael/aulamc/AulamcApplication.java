b9c6d732693b261e24037c7493bbd069c1e8847d
MikeDRei <michaelalvesreino@outlook.com>
2020-08-10 20:19:13 -0300

    Projeto Criado

---
+++ src/main/java/com/michael/aulamc/AulamcApplication.java
@@ -0,0 +1,13 @@
+package com.michael.aulamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamcApplication.class, args);
+	}
+
+}
